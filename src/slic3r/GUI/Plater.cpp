738d7cac7907a13e5c3ac4e86206be0ad519631a
Haotian Wu <hw2220@cloud-vm-42-134.doc.ic.ac.uk>
2023-06-30 23:38:35 +0100

    made platter textured plate only

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -1050,7 +1050,7 @@ void Sidebar::update_all_preset_comboboxes()
             p_mainframe->load_printer_url(url);
         }
 
-        m_bed_type_list->SelectAndNotify(btPEI-1);
+        m_bed_type_list->SelectAndNotify(btPTE);
         m_bed_type_list->Disable();
     }
 
