c45c70ac4b166df0bfa73932df5742ecb299e9a0
ohmdelta <ohmdelta@github.com>
2023-09-24 19:23:42 +0100

    update main

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -1182,7 +1182,7 @@ void Sidebar::update_all_preset_comboboxes()
         }
         p_mainframe->set_print_button_to_default(print_btn_type);
 
-        m_bed_type_list->SelectAndNotify(btPEI-1);
+        m_bed_type_list->SelectAndNotify(btPTE);
         m_bed_type_list->Disable();
     }
 
